1fcf2cffa70a45a49c52fbe4bd86065daf9c52cf
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-02-28 15:32:47 +0100

    Add router

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,10 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>'Laravel'</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+         <!-- Fonts -->
+         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -19,10 +17,12 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-    </head>
-    <body>
 
-<h1>Projet du 28/02/22 - Reprise Laravel PHP</h1>
+    <title>Liste des produits</title>
+</head>
+<body>
+
+<h1>C'est la liste des produits</h1>
 
 </body>
 </html>
